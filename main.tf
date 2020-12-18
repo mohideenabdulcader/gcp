30404cc8fc7ac841fa0a834ef9139283dc42cd30
mohideenabdulcader <cadertechy@gmail.com>
2020-12-18 03:02:18 +0000

    build commit

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 provider "google" {
   project     = "unilever-poc"
   region      = "us-central1"
-  credentials = file ("gcp-ulpoc.json")
+  #credentials = file ("gcp-ulpoc.json")
   zone = "us-east4-a"
 }
 terraform {
